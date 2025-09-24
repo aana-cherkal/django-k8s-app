6c44e35a86f5b6cd2e50c4cf276ce564a2e07e54
Aahana Cherkal <aahana.cherkal@gmail.com>
2025-09-24 17:09:48 +0530

    Adding django + docker + github workflow

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uiwrzjjc@rt1k*!(651ib%8y746)-wttt*b)r2c838q!w(+dr8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
